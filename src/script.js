72af7ccfdbcb33366e9fd94cf694080873f462f2
NikolaV91 <vasic.nikol@outlook.com>
2022-07-12 00:05:49 +0200

    create truth or dare game

---
+++ src/script.js
@@ -0,0 +1,244 @@
+var arrIstina = ["Koja je najveća laž koju ti je netko rekao u djetinjstvu?",
+    "Za koga se voliš udati?",
+    "Jeste li u djetinjstvu imali zamišljenog prijatelja?",
+    "Navedi bilo koje tri stvari o kojima razmišljaš dok sjediš na WC školjci.",
+    "Dajte ime svom dečku / djevojci.",
+    "Dajte ime posljednjoj stvari koju ste pretraživali na Googleu.",
+    "Volite li pjevati dok se tuširate?",
+    "Da li si ikada bio zaljubljen?",
+    "Koju ste supersilu ikad poželjeli imati?",
+    "Jeste li ikad nekoga predložili?",
+    "Otkrijte nam svoju najveću neugodnu tajnu.",
+    "Tko je tvoj najbolji prijatelj?",
+    "Imenujte bilo koga od svojih prijatelja kojima želite večerati uz svijeće.",
+    "Koja je najveća laž koju si nekome rekao?",
+    "Navedi bilo koju od svojih najgorih navika kojih se želiš riješiti.",
+    "Dajte imena najslađem dječaku ili djevojčici u vašem razredu?",
+    "Koga najviše voliš? Mama ili tata?",
+    "Jesu li vas ikad uhvatili kako razgovarate dok spavate?",
+    "Tko je tvoja djevojka iz snova / dječak iz snova?",
+    "Navedi svoj najveći strah.",
+    "Jeste li ikad slučajno vidjeli roditelje kako seksaju?",
+    "Jeste li ikad javno dirali no?",
+    "Dajte ime vašoj trenutnoj simpatiji.",
+    "Nazovite svoju simpatiju iz djetinjstva.",
+    "Jeste li se ikad zaljubili u svog učitelja?",
+    "Jeste li ikad poljubili svoju fotografiju?",
+    "Jesu li vas ikad uhvatili u šetnji dok spavate?",
+    "Jeste li ikad htjeli nekoga ubiti?",
+    "Navedi bilo koji Disneyev lik koji želiš postati",
+    "Spomenite svoju veličinu grudnjaka.",
+    "Jeste li ikad probali flert s dečkom / djevojkom svog najboljeg prijatelja?",
+    "Navedite bilo koju slavnu osobu koja vam se sviđa.",
+    "Kolika je vaša tjelesna težina?",
+    "Ako ikada dobijete priliku vjenčati se sa slavnom osobom, tko bi on bio?",
+    "Opišite najsmješniju zafrkanciju koju vam je netko ikad igrao?",
+    "Volite li lizati tanjur nakon što završite s obrokom?",
+    "Jeste li ikad probali hranu za mačke ili pse?",
+    "Koji je bio vaš nadimak u djetinjstvu?",
+    "Kakvu ste frizuru oduvijek željeli imati?",
+    "Navedi bilo koju životinju koja ti najviše sliči.",
+    "Koji je vaš položaj u karijeri iz snova?",
+    "Što prvo primijetite kod djevojčice ili dječaka?",
+    "Što najviše voliš? Čaj ili kava?",
+    "Smatrate li se seksi?",
+    "Jeste li ikad plakali zbog toga što ste nekoga propustili?",
+    "Jeste li ikad zamolili nekoga da izađe s vama na večeru?",
+    "Kada ste zadnji put plakali i zašto?",
+    "Plešete li kad ste sami?",
+    "Dajte ime bilo kojem dijelu tijela koji želite razmijeniti sa svojim najboljim prijateljem.",
+    "Navedi jednu stvar koju želiš promijeniti u sebi.",
+    "Navedi najgore poljupce prisutne u ovoj sobi.",
+    "Na koga osjećate ljubomoru?",
+    "Spomenite svoj struk.",
+    "Jeste li ikad poželjeli promijeniti spol?",
+    "Koji je tvoj posao iz snova?",
+    "Koji je najnekorisniji dio znanja?",
+    "Imenujte nekoga u sobi u koga ste potajno zaljubljeni.",
+    "Što je napisano na posljednjoj stranici vašeg dnevnika?",
+    "Opišite svoj prvi dojam o svom dečku ili djevojci.",
+    "Jeste li ikad namjerno vodili knjižničnu knjigu?"];
+
+var arrIzazov = ["Mahnite neznancu na cesti.",
+    "Pjevajte poput operne pjevačice.",
+    "Pokupi nos u javnosti.",
+    "Izađite vani i glasno zapjevajte himnu.",
+    "Oponašajte svog omiljenog Disneyevog lika.",
+    "Nazovite svoju simpatiju da biste priznali svoju ljubav.",
+    "Poljubi donji dio cipele svog prijatelja.",
+    "Poližite prednju gumu bicikla.",
+    "Idite uživo na Facebooku i recite ljudima glup vic.",
+    "Pojedi komad papira.",
+    "Pojedite žlicu kiselog krastavca.",
+    "Zagrlite drvo i poljubite mu lišće.",
+    "Zamolite susjeda za šalicu kave.",
+    "Pokušajte zvučati poput gladnog psa.",
+    "Sisajte palac petnaest minuta.",
+    "Nazovite mamu i recite joj koliko volite pušiti cigarete.",
+    "Neka vam ljudi bacaju jaja na lice.",
+    "Nazovite tatu i recite mu da imate dečka / djevojku.",
+    "Pozirajte poput modela Victoria Secreta.",
+    "Sljedećih deset minuta razgovarajte s rukom.",
+    "Izvedite 20 sklekova.",
+    "Njušite pazuhe osobe koja sjedi s vaše desne strane.",
+    "Nazovite nasumični broj i otpjevajte uspavanku primatelju poziva.",
+    "Pojedite žlicu šećera.",
+    "Ostatak igre igrajte u stojećem položaju.",
+    "Ostatak igre nosite grudnjak na košulji.",
+    "Oponašaj svoju mamu.",
+    "Nazovite neku slučajnu osobu i poželite „Sretan rođendan”.",
+    "Nazovite svoju simpatiju i predložite ga.",
+    "Nazovite pizzeriju i naručite pizzu za sve u sobi.",
+    "Nazovite svog učitelja i priznajte da ste varali na prošlom tečaju.",
+    "Nosite donje rublje na glavi do kraja igre.",
+    "Zagrlite toplo osobu koja sjedi s vaše desne strane.",
+    "Neka svi pogledaju vašu povijest pretraživanja na vašem telefonu.",
+    "Nazovite majku seksi glasom.",
+    "Zamolite osobu s vašeg prava da vas povuče za obraze.",
+    "Razmijenite donje rublje s osobom koja sjedi do vas.",
+    "Zubnim koncem zubite osobi koja sjedi s lijeve strane.",
+    "Naručite prsten putem interneta i poklonite ga svojoj majci.",
+    "Pozvonite na vrata svojih susjeda i bježite.",
+    "Izvedite Moonwalk.",
+    "Obrijajte pazuhe.",
+    "Grickajte sličice zubima.",
+    "Koketirajte sa slučajnim tipom na cesti.",
+    "Ispuni smiješne, prdeće zvukove.",
+    "Poljubite bilo koju osobu koja vam se sviđa u grupi vašeg prijatelja.",
+    "Nacrtajte tetovažu na desnoj ruci trajnim markerom.",
+    "Otuširajte se hladnim tušem i ostatak igre igrajte u mokroj odjeći.",
+    "Razgovaraj glasom patke.",
+    "Pustite porno video i pogledajte ga.",
+    "Pojedite šaku začina u prahu.",
+    "Depilirajte leđa bilo kojoj osobi.",
+    "Omirišite crni papar i nemojte kihati.",
+    "Ostatak igre igrajte zatvorenih očiju.",
+    "Pomiješajte šećer i crni papar i pojedite ih.",
+    "Utrljajte čokoladu na nos i pokušajte je lizati.",
+    "Objavite status na Facebooku samo laktima.",
+    "Stavite kockice leda u cipele i nosite ih do kraja igre.",
+    "Lizni lakat.",
+    "Oponašajte svog obiteljskog ljubimca.",
+    "Popijte deset čaša vode u jednom potezu."]
+
+var arrTruth = [
+    "When was the last time you lied?",
+    "When was the last time you cried?",
+    "What's your biggest fear?",
+    "What's your biggest fantasy?",
+    "Do you have any fetishes?",
+    "What's something you're glad your mum doesn't know about you?",
+    "Have you ever cheated on someone?",
+    "What's the worst thing you've ever done?",
+    "What's a secret you've never told anyone?",
+    "Do you have a hidden talent?",
+    "Who was your first celebrity crush?",
+    "What are your thoughts on polyamory?",
+    "What's the worst intimate experience you've ever had?",
+    "Have you ever cheated in an exam?",
+    "What's the most drunk you've ever been?",
+    "Have you ever broken the law?",
+    "What's the most embarrassing thing you've ever done?",
+    "What's your biggest insecurity?",
+    "What's the biggest mistake you've ever made?",
+    "What's the most disgusting thing you've ever done?",
+    "Who would you like to kiss in this room?",
+    "What's the worst thing anyone's ever done to you?",
+    "Have you ever had a run in with the law?",
+    "What's your worst habit?",
+    "What's the worst thing you've ever said to anyone?",
+    "Have you ever peed in the shower?",
+    "What's the strangest dream you've had?",
+    "Have you ever been caught doing something you shouldn't have?",
+    "What's the worst date you've been on?",
+    "What's your biggest regret?",
+    "What's the biggest misconception about you?",
+    "Where's the weirdest place you've had sex?",
+    "Why did your last relationship break down?",
+    "Have you ever lied to get out of a bad date?",
+    "What's the most trouble you've been in?"];
+
+var arrDare = [
+    "Show the most embarrassing photo on your phone",
+    "Show the last five people you texted and what the messages said",
+    "Let the rest of the group DM someone from your Instagram account",
+    "Eat a raw piece of garlic",
+    "Do 100 squats",
+    "Keep three ice cubes in your mouth until they melt",
+    "Say something dirty to the person on your left",
+    "Give a foot massage to the person on your right",
+    "Put 10 different available liquids into a cup and drink it",
+    "Yell out the first word that comes to you mind",
+    "Give a lap dance to someone of your choice",
+    "Remove four items of clothing",
+    "Like the first 15 posts on your Facebook newsfeed",
+    "Eat a spoonful of mustard",
+    "Keep your eyes closed until it's your go again",
+    "Send a sext to the last person in your phonebook",
+    "Show off your orgasm face",
+    "Seductively eat a banana",
+    "Empty out your wallet / purse and show everyone what's inside",
+    "Do your best sexy crawl",
+    "Pretend to be the person to your right for 10 minutes",
+    "Eat a snack without using your hands",
+    "Say two honest things about everyone else in the group",
+    "Twerk for a minute",
+    "Try and make the group laugh as quickly as possible",
+    "Try to put your whole fist in your mouth",
+    "Tell everyone an embarrassing story about yourself",
+    "Try to lick your elbow",
+    "Post the oldest selfie on your phone on Instagram Stories",
+    "Tell the saddest story you know",
+    "Howl like a wolf for two minutes",
+    "Dance without music for two minutes",
+    "Pole dance with an imaginary pole",
+    "Let someone else tickle you and try not to laugh",
+    "Put as many snacks into your mouth at once as you can"];
+
+var flag = true;
+var langFlag = false;
+
+var label = document.querySelector("#label");
+var btn = document.querySelector("#button1");
+var btn2 = document.querySelector("#button2");
+var nazivIgre = document.querySelector("#labelNaziv");
+var btnEngLang = document.querySelector("#engLang");
+var btnSerbLang = document.querySelector("#serbLang");
+
+btn.addEventListener("click", () => {
+    console.log("good")
+    return label.innerHTML = (flag === true ? arrIzazov[Math.floor(Math.random() * arrIzazov.length)].toUpperCase()
+        : arrDare[Math.floor(Math.random() * arrDare.length)].toUpperCase())
+})
+
+btn2.addEventListener("click", () => {
+    return label.innerHTML = (flag === true ? arrIstina[Math.floor(Math.random() * arrIstina.length)].toUpperCase()
+        : arrTruth[Math.floor(Math.random() * arrTruth.length)].toUpperCase())
+
+})
+
+btnEngLang.addEventListener("click", event1 => {
+    flag = false;
+    langFlag = true;
+    console.log(langFlag)
+    btn.innerHTML = "DARE";
+    btn2.innerHTML = "TRUTH";
+    nazivIgre.innerHTML = "TRUTH OR DARE";
+    label.innerHTML = "Lets Play!";
+    btnEngLang.classList.toggle("toggleClass");
+    langFlag === false ? btnEngLang.classList.add("toggleClass") : null;
+    btnSerbLang.classList.remove("toggleClass");
+    console.log(arrIstina.length)
+})
+
+btnSerbLang.addEventListener("click", event2 => {
+    flag = true;
+    langFlag = false;
+    console.log(langFlag)
+    btn.innerHTML = "IZAZOV";
+    btn2.innerHTML = "ISTINA";
+    nazivIgre.innerHTML = "ISTINA ILI IZAZOV";
+    label.innerHTML = "Igrajmo se!";
+    langFlag === false ? btnSerbLang.classList.add("toggleClass") : null;
+    btnEngLang.classList.remove("toggleClass");
+})
